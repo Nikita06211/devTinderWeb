cc83bbf2be62023e4bd40f08f5250edb6795a6df
Nikita <bansalnikita06@gmail.com>
2025-03-15 12:07:35 +0530

    installed react-redux, created userSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import NavBar from './NavBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Body from './Body';
 import Profile from './Profile';
 import Login from './Login';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore'; 
+
 
 function App() {
   return (
     <>
-      <BrowserRouter basename='/'>
-        <Routes>
-          <Route path='/' element = {<Body/>}>
-            <Route path='/login' element = {<Login/>}/>
-            <Route path='/profile' element = {<Profile/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store = {appStore}>
+        <BrowserRouter basename='/'>
+          <Routes>
+            <Route path='/' element = {<Body/>}>
+              <Route path='/login' element = {<Login/>}/>
+              <Route path='/profile' element = {<Profile/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
-
-export default App
+export default App;
