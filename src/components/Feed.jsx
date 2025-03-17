9b858960f881b8af2976d5791dcb6d97074afc5b
Nikita <bansalnikita06@gmail.com>
2025-03-17 20:48:29 +0530

    feat: send/ignore connection request

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,28 +7,36 @@ import UserCard from './UserCard';
 
 const Feed = () => {
   const dispatch = useDispatch();
-  const feed = useSelector((store)=>store.feed);
+  const feed = useSelector((store) => store.feed);
 
-  const getFeed = async ()=>{
-    if(feed) return;
-    try{
-      const res = await axios.get(BASE_URL+"/feed",{
-        withCredentials:true,
+  const getFeed = async () => {
+    if (feed.length > 0) return;  // Avoid fetching if feed already exists
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
       });
       dispatch(addFeed(res.data));
-    }catch(err){
+    } catch (err) {
       console.error(err.message);
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getFeed();
-  },[]);
+  }, []);
+
+  // Prevent rendering if feed is empty or undefined
+  if (!feed || feed.length === 0) {
+    return <p>Loading...</p>; 
+  }
+
   return (
-     feed && (<div className='flex justify-center my-3'>
-      <UserCard user = {feed[9]}/>
-    </div>)
-  )
+    <>
+      <div className="flex justify-center mb-20 mt-3">
+        <UserCard user={feed[0]} />
+      </div>
+    </>
+  );
 };
 
-export default Feed
+export default Feed;
